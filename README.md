58289a62e9a29d90ac039d2d89b66d7a0b8e0c67
Ben Kraft <ben.kraft@rcn.com>
2025-06-30 17:10:29 -0400

    Add image conversion, sample library

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sankeymosaic
+# SankeyMosaic 📊➡️🖼️
+
 Turns popular Sankey diagram generator sankeymatic.com into an image display.
+
+## Usage
+
+To create an image with this generator, your image needs to be places within the `samples/` directory and specified on program run.
+
+The resulting chart information can be found in `output.txt`, which can then be loaded into the site.
--- main.py
+++ main.py
@@ -2,35 +2,64 @@ from pathlib import Path
 
 from PIL import Image
 
-DEFAULT_SCALE = 100
+DEFAULT_SCALE = 7.5
 
 def main() -> None:
-        
+    """
+    Run main action of mosiac maker.
+    """
+    # Create directory path
     directory_path = Path(__file__).parent
-    # Opens settings file to read
+    # Create image path
+    image_path = directory_path / "samples" / "craig.png"
+
+    # Open settings file to read
     with open(directory_path / "settings.txt") as settings_file:
-        # Opens output file to write
+        # Open output file to write
         with open(directory_path / "output.txt", "w") as output_file:
-            # Opens image file to read
-            with Image.open(directory_path / "sample.png") as image:
-                # Writes custom image data to output file
+            # Open image file to read
+            with Image.open(image_path) as image:
+                # Write custom image data to output file
                 output_file.writelines(get_flows(image))
-            # Writes default settings to output file
+            # Write default settings to output file
             output_file.writelines(settings_file.readlines())
 
 
-
-def get_flows(image: Image.Image, scale: int = DEFAULT_SCALE) -> list[str]:
+def get_flows(image: Image.Image, scale: float = DEFAULT_SCALE) -> list[str]:
     """
-    Gets custom data from image and converts to flow data.
+    Get custom data from image and converts to flow data.
     """
     custom_lines = []
+    # Get largest pixel ID possible
+    max_id = len(str(image.width * image.height - 1))
+    # Load pixel data from image
+    pixels = image.load()
 
-    custom_lines.append("D [1] E\nE [1] F\n\n")
-
-    width, height = (axis * scale for axis in image.size)
+    id = 0
+    # For each vertical pixel:
+    for y in range(image.height):
+        # For each horizontal pixel:
+        for x in range(image.width):
+            # Acquire RGB pixel from image data
+            rgb_color: tuple[int] = pixels[x, y] # type: ignore
+            # Remove transparency data
+            if len(rgb_color) > 3:
+                rgb_color = rgb_color[:3]
+            # Convert pixel color from RGB to HEX
+            hex_color = "#%02x%02x%02x" % rgb_color # type: ignore
+            # Build string from ID to next
+            custom_lines.append(f"{id:0{max_id}d} [1] {(id + 1):0{max_id}d} {hex_color}\n")
+            # Add to ID
+            id += 1
+        # Carriage return by starting from new ID
+        custom_lines.append("\n")
+        id += 1
+    # Add seperator
+    custom_lines.append("\n")
+    # Scale image width and height before adding to settings
+    width, height = (int(axis * scale) for axis in image.size)
     custom_lines.append(f"size w {width}\n  h {height}\n")
-
+    # Return built string list
     return custom_lines
 
 
