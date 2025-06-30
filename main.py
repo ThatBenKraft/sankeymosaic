f3327a17390de75bf7e74d4ebb56f627221103be
Ben Kraft <ben.kraft@rcn.com>
2025-06-30 15:38:10 -0400

    Add basic file functionality

--- main.py
+++ main.py
@@ -2,8 +2,37 @@ from pathlib import Path
 
 from PIL import Image
 
-image_path = Path(__file__).parent / "sample.png"
+DEFAULT_SCALE = 100
 
-with Image.open(image_path) as image:
+def main() -> None:
+        
+    directory_path = Path(__file__).parent
+    # Opens settings file to read
+    with open(directory_path / "settings.txt") as settings_file:
+        # Opens output file to write
+        with open(directory_path / "output.txt", "w") as output_file:
+            # Opens image file to read
+            with Image.open(directory_path / "sample.png") as image:
+                # Writes custom image data to output file
+                output_file.writelines(get_flows(image))
+            # Writes default settings to output file
+            output_file.writelines(settings_file.readlines())
 
-    print("hi")
+
+
+def get_flows(image: Image.Image, scale: int = DEFAULT_SCALE) -> list[str]:
+    """
+    Gets custom data from image and converts to flow data.
+    """
+    custom_lines = []
+
+    custom_lines.append("D [1] E\nE [1] F\n\n")
+
+    width, height = (axis * scale for axis in image.size)
+    custom_lines.append(f"size w {width}\n  h {height}\n")
+
+    return custom_lines
+
+
+if __name__ == "__main__":
+    main()
